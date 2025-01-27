740dff6b929dedc8e354d6ed7e688c6143766cd6
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-01-27 14:26:16 +0100

    dynamic version

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
-use pyo3::prelude::*;
+// Rust module
 
-#[pyfunction]
-fn hello_from_bin() -> String {
-    "Hello from aq-models!".to_string()
-}
-
-/// A Python module implemented in Rust. The name of this function must match
-/// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
-/// import the module.
-#[pymodule]
-fn _core(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(hello_from_bin, m)?)?;
-    Ok(())
-}
+// Python bindings to rust module
+#[cfg(feature = "py")]
+pub mod py_bindings;
