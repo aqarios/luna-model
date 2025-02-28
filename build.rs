609f905095bd0802d357f0abf1114ac1db65032a
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-02-28 17:21:01 +0100

    removed prost-build, as we implement the structs by hand, not relying on the .proto files

--- build.rs
+++
@@ -1,8 +0,0 @@
-// extern crate prost_build;
-
-fn main() {}
-
-// fn main() -> std::io::Result<()> {
-//     // prost_build::compile_protos(&["src/proto/model.proto"], &["src/"])?;
-//     // Ok(())
-// }
