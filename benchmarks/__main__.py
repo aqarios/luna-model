ce57f9ec51116ff7f49650b22bb88dad806716ac
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-03-05 18:13:40 +0100

    updated bench

--- benchmarks/__main__.py
+++ benchmarks/__main__.py
@@ -31,26 +31,24 @@ def header():
         report_file_write.write("```")
 
 
-def footer():
+def footer(n: int = 1):
     if report_file_append:
         report_file_append.write("```")
+        rem: int = n - 1
+        for _ in range(rem):
+            report_file_append.write("\n")
 
 
 header()
 bench_read_dense_qubo(report_file_append)
-footer()
-footer()
+footer(2)
 bench_serialize_bqm(report_file_append)
-footer()
-footer()
+footer(2)
 bench_deserialize_bqm(report_file_append)
-footer()
-footer()
+footer(2)
 bench_serialize_bqm_size(report_file_append)
-footer()
-footer()
+footer(2)
 bench_serialize_aqm_xl(report_file_append)
-footer()
-footer()
+footer(2)
 bench_read_dense_qubo_memory(report_file_append)
 footer()
