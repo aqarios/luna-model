d26af979ad907ac8094d7ab5279b2c74f2ee39f1
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-03-10 13:53:38 +0100

    chore: minor update

--- src/translator/matrix_translator.rs
+++ src/translator/matrix_translator.rs
@@ -1,7 +1,21 @@
+use core::fmt;
+
 use crate::core::{
     expression::{BiasConstraints, IndexConstraints},
-    Model, Vtype,
+    ExpressionBase, Model, Vtype,
 };
+
+#[derive(Debug, Clone)]
+pub struct ModelNotQuadraticError;
+impl std::error::Error for ModelNotQuadraticError {}
+impl fmt::Display for ModelNotQuadraticError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "the model is not linear or quadratic, thus cannot be translated to a matrix."
+        )
+    }
+}
 
 pub struct MatrixTranslator {}
 
@@ -18,4 +32,25 @@ impl MatrixTranslator {
     {
         Model::new_from_dense(name, dense, num_variables, vtype)
     }
+
+    // pub fn model_to_dense<Index, Bias>(
+    //     model: Model<Index, Bias>,
+    // ) -> Result<Vec<Bias>, ModelNotQuadraticError>
+    // where
+    //     Index: IndexConstraints,
+    //     Bias: BiasConstraints,
+    // {
+    //     if model.objective.borrow().has_higher_order() {
+    //         return Err(ModelNotQuadraticError);
+    //     }
+
+    //     let nvars = model.objective.borrow().num_variables();
+    //     let flat: Vec<Bias> = Vec::with_capacity(nvars * nvars);
+
+    //     for (u, bias) in model.objective.borrow().linear.iter() {}
+
+    //     for (u, v, bias) in model.objective.borrow().quadratic.iter_flat() {}
+
+    //     Ok(flat)
+    // }
 }
