3f19226fd288f076e191332da4dcdf1fda481fac
Benjamin Sühling <83377820+bsuehling@users.noreply.github.com>
2025-03-18 11:44:48 +0100

    Add docstrings for solution structs

--- src/core/solution/sol.rs
+++ src/core/solution/sol.rs
@@ -29,6 +29,7 @@ where
 //     constraint_metadata: Rc<Option<ConstraintMetadata<Bias>>>,
 // }
 
+/// A result is a view into a certain sample of a solution and its corresponding metadata.
 #[derive(Debug, Clone)]
 pub struct Res<'a, Assignment, Bias>
 where
@@ -45,6 +46,7 @@ where
     /// Whether all constraints are satisfied
     feasible: Option<bool>,
     /// Constraint metadata, e.g., to show why a constraint is not feasible.
+    /// TODO: we need a vec here in order to have metadata for each single constraint.
     constraint_metadata: &'a Rc<Option<ConstraintMetadata<Bias>>>,
 }
 
@@ -116,21 +118,43 @@ pub struct Runtime {
 //     best_sample_idx: Option<usize>,
 // }
 
+/// The solutions object for AQMs. It doesn't have any knowledge about the corresponding AQM or
+/// about the environment the model was created in. Instead, for each sample, we expect the indices
+/// of the solution to be aligned with the variable indices of the model's environment.
 #[derive(Debug, Clone, Default)]
 pub struct Solution<Assignment, Bias>
 where
     Assignment: AssignmentConstraints,
     Bias: BiasConstraints,
 {
+    /// A collection of samples. Each inner vec corresponds to a single sample, i.e., an assignment
+    /// of a value to each model variable. `samples.len()` can be expected to always correspond
+    /// exactly to the number of results available in the solution.
     pub samples: Vec<Vec<Assignment>>,
+    /// How often each result occurs in the solution. `num_occurrences.len()` can be expected to
+    /// always be equal to `samples.len()`
     pub num_occurrences: Vec<usize>,
+    /// Objetive values as computed by the corresponding AQM. May be empty for solutions that
+    /// haven't yet been evaluated.
     pub obj_values: Vec<Bias>,
+    /// Objetive values as computed by the solver. May be empty if the solver does not provide
+    /// energies in its solution format. May be different from `obj_values`, e.g., because an offset
+    /// was neglected, or the AQM was transformed before being solved.
     pub raw_energies: Vec<Bias>,
-    /// Boolean flag for each single constraint whether it's satisfied
+    /// Boolean flag for each single constraint whether it's satisfied. Each inner vec corresponds
+    /// to one sample, i.e., `constraints[i]` corresponds to `samples[0]`. May be empty for
+    /// solutions that haven't yet been evaluated.
     constraints: Vec<Vec<bool>>,
-    /// Boolean flag for each sample whether it's feasible, i.e., all constraints are satisfied
+    /// Boolean flag for each sample whether it's feasible, i.e., whether all constraints are
+    /// satisfied. In other words, `feasible[i]` iff. `all(constraints[i])`. May be empty for
+    /// solutions that haven't yet been evaluated.
     feasible: Vec<bool>,
+    /// Metadata that may be useful for explaining why a constraint is not satisfied, e.g., the eval
+    /// of a lhs.
+    /// TODO: we need a Vec<Vec<_>> instead of a Vec<_> as each constraint needs these metadata.
     constraint_metadata: Vec<Option<ConstraintMetadata<Bias>>>,
+    /// The index of the sample with the lowest objective value or, if not present, of the sample
+    /// with the lowes raw energy. None, if none of these values are present.
     best_sample_idx: Option<usize>,
 }
 
