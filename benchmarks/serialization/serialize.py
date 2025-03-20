a94219785469317b5f50f27f670a6e0a7839f3ee
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-03-20 17:41:00 +0100

    benchmark fixes

--- benchmarks/serialization/serialize.py
+++ benchmarks/serialization/serialize.py
@@ -33,7 +33,7 @@ def bench_serialize_cqm(file: IO | None):
             qubo = make_qubo(size, density)
 
             aqm = MatrixTranslator.to_model(qubo, vtype=Vtype.Binary)
-            dmd = dimod.ConstrainedQuadraticModel.from_cqm(
+            dmd = dimod.ConstrainedQuadraticModel.from_bqm(
                 dimod.BinaryQuadraticModel(qubo, "BINARY")
             )
 
@@ -57,7 +57,7 @@ def bench_serialize_cqm_size(file: IO | None):
             qubo = make_qubo(size, density)
 
             aqm = MatrixTranslator.to_model(qubo, vtype=Vtype.Binary)
-            dmd = dimod.ConstrainedQuadraticModel.from_cqm(
+            dmd = dimod.ConstrainedQuadraticModel.from_bqm(
                 dimod.BinaryQuadraticModel(qubo, "BINARY")
             )
 
