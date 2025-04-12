1c434bfd8ee7d2ba44ab2855253c8a4a0ba55778
Benjamin Sühling <83377820+bsuehling@users.noreply.github.com>
2025-04-12 13:51:28 +0200

    Add docs for everything solution related

--- src/aqmodels/_result.py
+++ src/aqmodels/_result.py
@@ -3,6 +3,20 @@ from aqmodels._api_utils import export, dispatched
 
 @export
 class ResultIterator:
+    """
+    An iterator over a solution's results.
+
+    Examples
+    --------
+    >>> from aqmodels import ResultIterator, Solution
+    >>> solution: Solution = ...
+    >>> results: ResultIterator = solution.results
+    >>> for result in results:
+    ...     result.sample
+    [0, -5, 0.28]
+    [1, -4, -0.42]
+    """
+
     @dispatched
     def __iter__(self):
         return
@@ -14,6 +28,32 @@ class ResultIterator:
 
 @export
 class Result:
+    """
+    A result object can be understood as a solution with only one sample.
+
+    It can be obtained by calling ``model.evaluate_sample`` for a single sample.
+
+    Most properties available for the solution object are also available for a result,
+    but in the singular form. For example, you can call ``solution.obj_values``, but
+    ``result.obj_value``.
+
+    Examples
+    --------
+    >>> from aqmodels import Model, Result, Solution
+    >>> model: Model = ...
+    >>> solution: Solution = ...
+    >>> sample = solution.samples[0]
+    >>> result = model.evaluate_sample(sample)
+    >>> result.obj_value
+    -109.42
+    >>> result.sample
+    [0, -5, 0.28]
+    >>> result.constraints
+    [True, False]
+    >>> result.feasible
+    False
+    """
+
     @dispatched
     def __str__(self):
         return
@@ -25,40 +65,56 @@ class Result:
     @property
     @dispatched
     def sample(self):
+        """Get the sample of the result."""
         return
 
     @property
     @dispatched
     def obj_value(self):
+        """Get the objective value of the result"""
         return
 
     @property
     @dispatched
     def constraints(self):
+        """
+        Get this result's feasibility values of all constraints. Note that
+        ``results.constraints[i]`` iff. ``model.constraints[i]`` is feasible for
+        this result."""
         return
 
     @property
     @dispatched
     def feasible(self):
+        """Return whether all constraint results are feasible for this result."""
         return
 
 
 @export
 class ResultView:
     """
-    A view into a single result of a solution.
+    A result view object serves as a view into one row of a solution object.
+
+    The ``Result`` class is readonly as it's merely a helper class for looking into a
+    solution's row, i.e., a single sample and this sample's metadata.
+
+    Most properties available for the solution object are also available for a result,
+    but in the singular form. For example, you can call ``solution.obj_values``, but
+    ``result.obj_value``.
 
     Examples
     --------
     >>> from aqmodels import ResultView, Solution
-    >>> sol: Solution = <...>
-    >>> res: ResultView = sol[0]
-    >>> str(res.obj_value)
-    '-109.42'
-    >>> str(res.sample)
-    '[1, 0, 0, 1, 10, -3, -5.1]'
-    >>> str(res.feasible)
-    'True'
+    >>> solution: Solution = ...
+    >>> result: ResultView = solution[0]
+    >>> result.obj_value
+    -109.42
+    >>> result.sample
+    [0, -5, 0.28]
+    >>> result.constraints
+    [True, False]
+    >>> result.feasible
+    False
     """
 
     @dispatched
@@ -76,7 +132,7 @@ class ResultView:
     @property
     @dispatched
     def sample(self):
-        """Get an iterator over the variable assignments of the result's samples."""
+        """Get the sample of the result."""
         return
 
     @property
@@ -98,7 +154,7 @@ class ResultView:
     @dispatched
     def raw_energy(self):
         """
-        Get the raw energy returned by the solver if present. This value is not
+        Get the raw energy returned by the algorithm if present. This value is not
         guaranteed to be accurate under consideration of the corresponding AqModel.
         """
         return
@@ -107,15 +163,13 @@ class ResultView:
     @dispatched
     def constraints(self):
         """
-        Get the feasibility of each single constraint of the model the solution was
-        created for."""
+        Get this result's feasibility values of all constraints. Note that
+        ``results.constraints[i]`` iff. ``model.constraints[i]`` is feasible for
+        this result."""
         return
 
     @property
     @dispatched
     def feasible(self):
-        """
-        Return whether all constraints of the model the solution was created for are
-        feasible.
-        """
+        """Return whether all constraint results are feasible for this result."""
         return
--- src/aqmodels/_timing.py
+++ src/aqmodels/_timing.py
@@ -3,48 +3,130 @@ from aqmodels._api_utils import export, dispatched
 
 @export
 class Timing:
+    """
+    The object that holds information about an algorithm's runtime.
+
+    This class can only be constructed using a ``Timer``. This ensures that a
+    ``Timing`` object always contains a start as well as an end time.
+
+    The ``qpu`` field of this class can only be set after constructing it with a timer.
+
+    Examples
+    --------
+    >>> from dwave.samplers.tree.solve import BinaryQuadraticModel    >>> from aqmodels import Model, Timer, Timing
+    >>> model = ... # third-party model
+    >>> algorithm = ... # third-party algorithm
+    >>> timer = Timer.start()
+    >>> sol = algorithm.run(model)
+    >>> timing: Timing = timer.stop()
+    >>> timing.qpu = sol.qpu_time
+    >>> timing.total_seconds
+    1.2999193
+    >>> timing.qpu
+    0.02491934
+    """
+
     @property
     @dispatched
     def start(self):
+        """The starting time of the algorithm."""
         return
 
     @property
     @dispatched
     def end(self):
+        """The end, or finishing, time of the algorithm."""
         return
 
     @property
     @dispatched
     def total(self):
+        """The difference of the end and start time."""
         return
 
     @property
     @dispatched
     def total_seconds(self):
+        """The total time in seconds an algorithm needed to run. Computed as the
+        difference of end and start time."""
         return
 
     @property
     @dispatched
     def qpu(self):
+        """The qpu usage time of the algorithm this timing object was created for."""
         return
 
     @qpu.setter
     @dispatched
     def qpu(self, value):
+        """
+        Set the qpu usage time.
+
+        Raises
+        ------
+        ValueError
+            If ``value`` is negative."""
         return value
 
     @dispatched
     def add_qpu(self, value: float):
+        """
+        Add qpu usage time to the qpu usage time already present. If the current value
+        is None, this method acts like a setter.
+
+        Parameters
+        ----------
+        value : float
+            The value to add to the already present qpu value.
+
+        Raises
+        ------
+        ValueError
+            If ``value`` is negative.
+        """
         return
 
 
 @export
 class Timer:
+    """
+    Used to measure the computation time of an algorithm.
+
+    The sole purpose of the ``Timer`` class is to create a ``Timing`` object in a safe
+    way, i.e., to ensure that the ``Timing`` object always holds a starting and
+    finishing time.
+
+    Examples
+    --------
+    Basic usage:
+    >>> from aqmodels import  Timer
+    >>> timer = Timer.start()
+    >>> solution = ... # create a solution by running an algorithm.
+    >>> timing = timer.stop()
+    """
+
     @dispatched
     @staticmethod
     def start():
+        """
+        Create a timer that starts counting immediately.
+
+        Returns
+        -------
+        Timer
+            The timer
+        """
         return
 
     @dispatched
     def stop(self):
+        """
+        Stop the timer, and get the resulting ``Timing`` object.
+
+        Returns
+        -------
+        Timing
+            The timing object that holds the start and end time.
+        """
         return
