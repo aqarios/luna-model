2eb558745bcbc2786e235d3c81f39bed6aed89c3
Benjamin Sühling <83377820+bsuehling@users.noreply.github.com>
2025-04-23 21:42:35 +0200

    Add more tests

--- pytests/test_core/test_str_repr/test_solution.py
+++ pytests/test_core/test_str_repr/test_solution.py
@@ -18,9 +18,9 @@ sol_str_2 = """{
 }"""
 
 
-@pytest.mark.str_repr
-def test_model():
-    sol = Solution.build(
+@pytest.fixture
+def solution(request) -> Solution:
+    return Solution.build(
         component_types=[
             Vtype.Binary,
             Vtype.Spin,
@@ -34,14 +34,33 @@ def test_model():
         num_occurrences=[1, 2, 3],
         raw_energies=[6.0, 5.0, 2.0],
     )
-    assert str(sol) == sol_str_1
 
-    sol_2 = Solution.build(
+
+@pytest.mark.str_repr
+@pytest.mark.parametrize("solution", [()], indirect=True)
+def test_sample(solution: Solution):
+    sample = list(solution.samples)[0]
+    assert str(sample) == "[1, 1, 2, 2.0]"
+
+
+@pytest.mark.str_repr
+@pytest.mark.parametrize("solution", [()], indirect=True)
+def test_samples(solution: Solution):
+    samples = solution.samples
+    assert str(samples) == sol_str_1
+
+
+@pytest.mark.str_repr
+@pytest.mark.parametrize("solution", [()], indirect=True)
+def test_model(solution: Solution):
+    assert str(solution) == sol_str_1
+
+    solution_2 = Solution.build(
         component_types=[Vtype.Binary] * 30,
         binary_cols=[[1, 0]] * 30,
     )
-    assert str(sol_2) == sol_str_2
+    assert str(solution_2) == sol_str_2
 
     with does_not_raise():
-        repr(sol)
-        repr(sol_2)
+        repr(solution)
+        repr(solution_2)
