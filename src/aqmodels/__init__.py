e3c1b4acd39d28e732e3a3816cd50ac9be6613ae
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-05-15 12:34:10 +0200

    chore: minor update

--- src/aqmodels/__init__.py
+++ src/aqmodels/__init__.py
@@ -48,70 +48,59 @@ transformers
     to a linear model.
 """
 
-from ._variable import Vtype, Bounds, Variable
+from ._variable import Variable, Bounds, Vtype
 from ._timing import Timer, Timing
 from ._solution import Solution
-from ._sample import SampleIterator, Sample, SamplesIterator, Samples
-from ._result import ResultView, Result, ResultIterator
-from ._model import Model, Sense
+from ._sample import SampleIterator, Samples, Sample, SamplesIterator
+from ._result import ResultIterator, ResultView, Result
+from ._model import Sense, Model
 from ._expression import Expression
 from ._environment import Environment
 from ._core import (
-    Sample as __Sample,
-    ResultIterator as __ResultIterator,
-    Sense as __Sense,
-    Comparator as __Comparator,
-    Bounds as __Bounds,
-    Samples as __Samples,
-    Timing as __Timing,
-    Expression as __Expression,
-    SampleIterator as __SampleIterator,
-    Model as __Model,
-    Constraints as __Constraints,
     Constraint as __Constraint,
-    SamplesIterator as __SamplesIterator,
-    Solution as __Solution,
     Timer as __Timer,
-    Vtype as __Vtype,
     Result as __Result,
+    Bounds as __Bounds,
+    Comparator as __Comparator,
+    Model as __Model,
+    Vtype as __Vtype,
+    Timing as __Timing,
+    SampleIterator as __SampleIterator,
     ResultView as __ResultView,
-    Variable as __Variable,
+    Sample as __Sample,
     Environment as __Environment,
+    Variable as __Variable,
+    Expression as __Expression,
+    Sense as __Sense,
+    Solution as __Solution,
+    SamplesIterator as __SamplesIterator,
+    Samples as __Samples,
+    Constraints as __Constraints,
+    ResultIterator as __ResultIterator,
 )
-from ._constraints import Constraints, Comparator, Constraint
+from ._constraints import Constraint, Comparator, Constraints
 from . import translator, errors
 
+Sense = __Sense  # type: ignore[misc,assignment] # noqa: F811
+Model = __Model  # type: ignore[misc,assignment] # noqa: F811
+Expression = __Expression  # type: ignore[misc,assignment] # noqa: F811
 Comparator = __Comparator  # type: ignore[misc,assignment] # noqa: F811
 Constraint = __Constraint  # type: ignore[misc,assignment] # noqa: F811
 Constraints = __Constraints  # type: ignore[misc,assignment] # noqa: F811
-Vtype = __Vtype  # type: ignore[misc,assignment] # noqa: F811
-Bounds = __Bounds  # type: ignore[misc,assignment] # noqa: F811
-Variable = __Variable  # type: ignore[misc,assignment] # noqa: F811
-Timing = __Timing  # type: ignore[misc,assignment] # noqa: F811
-Timer = __Timer  # type: ignore[misc,assignment] # noqa: F811
-Sense = __Sense  # type: ignore[misc,assignment] # noqa: F811
-Model = __Model  # type: ignore[misc,assignment] # noqa: F811
 ResultIterator = __ResultIterator  # type: ignore[misc,assignment] # noqa: F811
 Result = __Result  # type: ignore[misc,assignment] # noqa: F811
 ResultView = __ResultView  # type: ignore[misc,assignment] # noqa: F811
-Solution = __Solution  # type: ignore[misc,assignment] # noqa: F811
-Environment = __Environment  # type: ignore[misc,assignment] # noqa: F811
 SamplesIterator = __SamplesIterator  # type: ignore[misc,assignment] # noqa: F811
 SampleIterator = __SampleIterator  # type: ignore[misc,assignment] # noqa: F811
 Samples = __Samples  # type: ignore[misc,assignment] # noqa: F811
 Sample = __Sample  # type: ignore[misc,assignment] # noqa: F811
-Expression = __Expression  # type: ignore[misc,assignment] # noqa: F811
-Qubo = translator.Qubo
-QuboTranslator = translator.QuboTranslator
-AwsTranslator = translator.AwsTranslator
-ZibTranslator = translator.ZibTranslator
-DwaveTranslator = translator.DwaveTranslator
-BqmTranslator = translator.BqmTranslator
-QctrlTranslator = translator.QctrlTranslator
-LpTranslator = translator.LpTranslator
-NumpyTranslator = translator.NumpyTranslator
-IbmTranslator = translator.IbmTranslator
-CqmTranslator = translator.CqmTranslator
+Vtype = __Vtype  # type: ignore[misc,assignment] # noqa: F811
+Bounds = __Bounds  # type: ignore[misc,assignment] # noqa: F811
+Variable = __Variable  # type: ignore[misc,assignment] # noqa: F811
+Environment = __Environment  # type: ignore[misc,assignment] # noqa: F811
+Timing = __Timing  # type: ignore[misc,assignment] # noqa: F811
+Timer = __Timer  # type: ignore[misc,assignment] # noqa: F811
+Solution = __Solution  # type: ignore[misc,assignment] # noqa: F811
 VariableOutOfRangeError = errors.VariableOutOfRangeError
 VariableExistsError = errors.VariableExistsError
 VariableCreationError = errors.VariableCreationError
@@ -131,6 +120,17 @@ ModelSenseNotMinimizeError = errors.ModelSenseNotMinimizeError
 SolutionTranslationError = errors.SolutionTranslationError
 SampleIncorrectLengthError = errors.SampleIncorrectLengthError
 SampleIncompatibleVtypeError = errors.SampleIncompatibleVtypeError
+BqmTranslator = translator.BqmTranslator
+QctrlTranslator = translator.QctrlTranslator
+ZibTranslator = translator.ZibTranslator
+AwsTranslator = translator.AwsTranslator
+IbmTranslator = translator.IbmTranslator
+NumpyTranslator = translator.NumpyTranslator
+DwaveTranslator = translator.DwaveTranslator
+LpTranslator = translator.LpTranslator
+Qubo = translator.Qubo
+QuboTranslator = translator.QuboTranslator
+CqmTranslator = translator.CqmTranslator
 
 __all__ = [
     "AwsTranslator",
