554f399cc66d139e885eb550cc323e2d7c60180b
Benjamin Sühling <83377820+bsuehling@users.noreply.github.com>
2025-05-19 12:40:41 +0200

    Copy docs for sol and timing

--- src/py_bindings/py_sol.rs
+++ src/py_bindings/py_sol.rs
@@ -32,6 +32,51 @@ pub enum SampleKey {
     Var(PyVariable),
 }
 
+/// The solution object that is obtained by running an algorihtm.
+///
+/// The ``Solution`` class represents a summary of all data obtained from solving a
+/// model. It contains samples, i.e., assignments of values to each model variable as
+/// returned by the algorithm, metadata about the solution quality, e.g., the objective
+/// value, and the runtime of the algorithm.
+///
+/// Models can't be constructed explicitly, but rather by obtaining a solution from an
+/// algorithm, or by converting a different solution format with one of the available
+/// translators. Note that the latter requires the environment the model was created
+/// in.
+///
+/// Examples
+/// --------
+/// Basic usage, assuming that the algorithm already returns a ``Solution``:
+///
+/// >>> from aqmodels import Model, Solution
+/// >>> model: Model = ...
+/// >>> algorithm = ...
+/// >>> solution: Solution = algorithm.run(model)
+/// >>> solution.samples
+/// [[1, 0, 1], [0, 0, 1]]
+///
+/// When you have a ``dimod.Sampleset`` as raw solution format:
+///
+/// >>> from aqmodels.translator import BqmTranslator    >>> from aqmodels import Model, Solution, DwaveTranslator
+/// >>> from dimod import SimulatedAnnealingSampler
+/// >>> model: Model = ...
+/// >>> bqm = BqmTranslator.from_aq(model)
+/// >>> sampleset = SimulatedAnnealingSampler().sample(bqm)
+/// >>> solution = DwaveTranslator.from_dimod_sample_set(sampleset)
+/// >>> solution.samples
+/// [[1, 0, 1], [0, 0, 1]]
+///
+/// Serialization:
+///
+/// >>> blob = solution.encode()
+/// >>> restored = Solution.decode(blob)
+/// >>> restored.samples
+/// [[1, 0, 1], [0, 0, 1]]
+///
+/// Notes
+/// -----
+/// - To ensure metadata like objective values or feasibility, use ``model.evaluate(solution)``.
+/// - Use ``encode()`` and ``decode()`` to serialize and recover solutions.
 #[pyclass(unsendable, name = "Solution", module = "aqmodels")]
 #[derive(Deref, DerefMut, Debug)]
 pub struct PySolution(pub RcSolution<ConcreteBias, ConcreteAssignmentTypes>);
@@ -44,6 +89,101 @@ impl Into<RcSolution<ConcreteBias, ConcreteAssignmentTypes>> for PySolution {
 
 #[pymethods]
 impl PySolution {
+    /// Build a Solution based on the provided input data. The solution is constructed
+    /// based on a column layout of the solution. Let's take the following sample-set with three
+    /// samples as an example:
+    ///
+    /// [ 0  1  -1  3  2.2  1 ]
+    /// [ 1  0  -1  6  3.8  0 ]
+    /// [ 1  1  +1  2  2.4  0 ]
+    ///
+    /// Each row encodes a single sample. However, the variable types vary, the first, second and last
+    /// columns all represent a Binary variable (index 0, 1, 5). The third column represents a variable
+    /// of type Spin (index 2). The fourth column (index 3) a variable of type Integer and the fith column
+    /// (index 4) a real valued variable.
+    ///
+    /// Thus, the `component_types` list is:
+    ///
+    /// >>> component_types = [Vtype.Binary, Vtype.Binary, Vtype.Spin, Vtype.Integer, Vtype.Real, Vtype.Binary]
+    ///
+    /// Now we can extract all columns for a binary valued variable and append them to a new list:
+    ///
+    /// >>> binary_cols = [[0, 1, 1], [1, 0, 1], [1, 0, 0]]
+    ///
+    /// where the first element in the list represents the first column, the second element the second
+    /// column and the third element the fith column.
+    /// We do the same for the remaning variable types:
+    ///
+    /// >>> spin_cols = [[-1, -1, +1]]
+    /// >>> int_cols = [[3, 6, 2]]
+    /// >>> real_cols = [[2.2, 3.8, 2.4]]
+    ///
+    /// If we know the raw energies we can construct them as well:
+    ///
+    /// >>> raw_energies = [-200, -100, +300]
+    ///
+    /// And finally call the `build` function:
+    ///
+    /// >>> sol = Solution.build(
+    /// ...     component_types,
+    /// ...     binary_cols,
+    /// ...     spin_cols,
+    /// ...     int_cols,
+    /// ...     real_cols,
+    /// ...     raw_energies,
+    /// ...     timing,
+    /// ...     counts=[1, 1, 1]
+    /// ... )
+    /// >>> sol
+    ///
+    /// In this example, we could also neglect the `counts` as it defaults to `1`
+    /// for all samples if not set:
+    ///
+    /// >>> sol = Solution.build(
+    /// ...     component_types,
+    /// ...     binary_cols,
+    /// ...     spin_cols,
+    /// ...     int_cols,
+    /// ...     real_cols,
+    /// ...     raw_energies,
+    /// ...     timing
+    /// ... )
+    /// >>> sol
+    ///
+    ///
+    /// Parameters
+    /// ----------
+    /// component_types : list[Vtype]
+    ///     The variable type each element in a sample encodes.
+    /// binary_cols : list[list[int]], optional
+    ///     The data of all binary valued columns. Each inner list encodes a single binary valued column.
+    /// Required if any element in the `component_types` is `Vtype.Binary`.
+    ///     spin_cols : list[list[int]], optional
+    /// The data of all spin valued columns. Each inner list encodes a single spin valued column.
+    ///     Required if any element in the `component_types` is `Vtype.Spin`.
+    /// int_cols : list[list[int]], optional
+    ///     The data of all integer valued columns. Each inner list encodes a single integer valued column.
+    /// Required if any element in the `component_types` is `Vtype.Integer`.
+    ///     real_cols : list[list[float]], optional
+    /// The data of all real valued columns. Each inner list encodes a single real valued column.
+    ///     Required if any element in the `component_types` is `Vtype.Real`.
+    /// raw_energies : list[float, optional], optional
+    ///     The data of all real valued columns. Each inner list encodes a single real valued column.
+    /// timing : Timing, optional
+    ///     The timing data.
+    /// counts : list[int], optional
+    ///     The number each sample in the solution has occurred. By default 1 for all samples.
+    ///
+    /// Returns
+    /// -------
+    /// Solution
+    ///     The constructed solution
+    ///
+    /// Raises
+    /// ------
+    /// RuntimeError
+    ///     If a sample column has an incorrect number of samples or if ``counts`` has
+    ///     a length different from the number of samples given.
     #[staticmethod]
     #[pyo3(signature=(component_types, binary_cols=None, spin_cols=None, int_cols=None, real_cols=None, raw_energies=None, timing=None, counts=None)
     )]
@@ -57,8 +197,8 @@ impl PySolution {
         timing: Option<PyTiming>,
         counts: Option<Vec<usize>>,
     ) -> PyResult<Self> {
-        // todo! change to numpy arrays instead of vecs.
-        // todo! move further down in rust code.
+        /// todo! change to numpy arrays instead of vecs.
+        /// todo! move further down in rust code.
         let mut sol = Solution::default();
 
         let (mut lb, mut ls, mut li, mut lr) = (0, 0, 0, 0);
@@ -130,6 +270,44 @@ impl PySolution {
         Ok(PySolution(RcSolution(Rc::new(sol))))
     }
 
+    /// Create a solution from a dict that maps variables or variable names to their
+    /// assigned values.
+    ///
+    /// If a Model is passed, the solution will be evaluated immediately. Otherwise,
+    /// there has to be an environment present to determine the correct variable types.
+    ///
+    /// Parameters
+    /// ----------
+    /// data : dict[Variable | str, int | float]
+    ///     The sample that shall be part of the solution.
+    /// env : Environment, optional
+    ///     The environment the variable types shall be determined from.
+    /// model : Model, optional
+    ///     A model to evaluate the sample with.
+    ///
+    /// Returns
+    /// -------
+    /// Solution
+    ///     The solution object created from the sample dict.
+    ///
+    /// Raises
+    /// ------
+    /// NoActiveEnvironmentFoundError
+    ///     If no environment or model is passed to the method or available from the
+    ///     context.
+    /// ValueError
+    ///     If `env` and `model` are both present. When this is the case, the user's
+    ///     intention is unclear as the model itself already contains an environment.
+    /// SolutionTranslationError
+    ///     Generally if the sample translation fails. Might be specified by one of the
+    ///     three following errors.
+    /// SampleIncorrectLengthErr
+    ///     If a sample has a different number of variables than the environment.
+    /// SampleUnexpectedVariableError
+    ///     If a sample has a variable that is not present in the environment.
+    /// ModelVtypeError
+    ///     If the result's variable types are incompatible with the model environment's
+    ///     variable types.
     #[staticmethod]
     #[pyo3(signature=(data, env=None, model=None)
     )]
@@ -201,16 +379,20 @@ impl PySolution {
         Ok(PySolution(sol_rc))
     }
 
+    /// Get an iterator over the single results of the solution.
     #[getter]
     fn results<'a>(&self) -> PyResultIterator {
         PyResultIterator(self.0.iter_results())
     }
 
+    /// Get a view into the samples of the solution.
     #[getter]
     fn samples(&self) -> PySamples {
         PySamples(Samples(RcSolution::clone(&self)))
     }
 
+    /// Get the objective values of the single samples as a ndarray. A value will be
+    /// None if the sample hasn't yet been evaluated.
     #[getter]
     fn obj_values<'a>(&self, py: Python<'a>) -> Bound<'a, PyArray1<PyObject>> {
         self.obj_values
@@ -220,6 +402,8 @@ impl PySolution {
             .to_pyarray(py)
     }
 
+    /// Get the raw energy values of the single samples as returned by the solver /
+    /// algorithm. Will be None if the solver / algorithms did not provide a value.
     #[getter]
     fn raw_energies<'a>(&self, py: Python<'a>) -> Bound<'a, PyArray1<PyObject>> {
         self.raw_energies
@@ -229,21 +413,42 @@ impl PySolution {
             .to_pyarray(py)
     }
 
+    /// Return how often each sample occurred in the solution.
     #[getter]
     fn counts<'a>(&self, py: Python<'a>) -> Bound<'a, PyArray1<usize>> {
         self.counts.to_pyarray(py)
     }
 
+    /// Get the solver / algorithm runtime.
     #[getter]
     fn runtime(&self) -> Option<PyTiming> {
         self.timing.map(|t| PyTiming(t))
     }
 
+    /// Get the index of the sample with the best objective value.
     #[getter]
     fn best_sample_idx(&self) -> Option<usize> {
         self.0.best_sample_idx
     }
 
+    /// Serialize the solution into a compact binary format.
+    ///
+    /// Parameters
+    /// ----------
+    /// compress : bool, optional
+    ///     Whether to compress the binary output. Default is True.
+    /// level : int, optional
+    ///     Compression level (0–9). Default is 3.
+    ///
+    /// Returns
+    /// -------
+    /// bytes
+    ///     Encoded model representation.
+    ///
+    /// Raises
+    /// ------
+    /// IOError
+    ///     If serialization fails.
     #[pyo3(signature=(compress=true, level=3))]
     fn encode(&self, py: Python, compress: Option<bool>, level: Option<i32>) -> PyResult<PyObject> {
         let compress = compress.unwrap_or(level.is_some());
@@ -255,9 +460,10 @@ impl PySolution {
                 .maybe_compress(compress, level)?
                 .versionize(),
         )
-        .into())
+            .into())
     }
 
+    /// Alias for ``encode()``.
     #[pyo3(signature=(compress=true, level=3))]
     fn serialize(
         &self,
@@ -268,6 +474,22 @@ impl PySolution {
         self.encode(py, compress, level)
     }
 
+    /// Reconstruct a solution object from binary data.
+    ///
+    /// Parameters
+    /// ----------
+    /// data : bytes
+    ///     Serialized model blob created by ``encode()``.
+    ///
+    /// Returns
+    /// -------
+    /// Solution
+    ///     The reconstructed solution.
+    ///
+    /// Raises
+    /// ------
+    /// DecodeError
+    ///     If decoding fails due to corruption or incompatibility.
     #[staticmethod]
     fn decode(py: Python, data: Py<PyBytes>) -> PyResult<Self> {
         Ok(PySolution(
@@ -275,6 +497,7 @@ impl PySolution {
         ))
     }
 
+    /// Alias for ``decode()``.
     #[staticmethod]
     fn deserialize(py: Python, data: Py<PyBytes>) -> PyResult<Self> {
         Self::decode(py, data)
@@ -288,10 +511,27 @@ impl PySolution {
         format!("{:#?}", self.0)
     }
 
+    /// Iterate over the single results of the solution.
+    ///
+    /// Returns
+    /// -------
+    /// ResultIterator
     fn __iter__(slf: PyRef<'_, Self>) -> PyResultIterator {
         PyResultIterator(slf.0.iter_results())
     }
 
+    /// Extract a result view from the ``Solution`` object.
+    ///
+    /// Returns
+    /// -------
+    /// ResultView
+    ///
+    /// Raises
+    /// ------
+    /// TypeError
+    ///     If ``item`` has the wrong type.
+    /// IndexError
+    ///     If the row index is out of bounds for the variable environment.
     fn __getitem__(&self, py: Python, item: PyObject) -> PyResult<PyResultView> {
         if let Ok(res_idx) = item.extract::<usize>(py) {
             match self.get_result_view(res_idx) {
@@ -305,6 +545,15 @@ impl PySolution {
         }
     }
 
+    /// Check whether this solution is equal to ``other``.
+    ///
+    /// Parameters
+    /// ----------
+    /// other : Model
+    ///
+    /// Returns
+    /// -------
+    /// bool
     fn __eq__(&self, other: &PySolution) -> bool {
         &self.0 == &other.0
     }
