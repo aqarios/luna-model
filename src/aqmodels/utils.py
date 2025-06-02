60624e02d365195aa8cbca7abbae0102d9555cf5
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-06-02 12:04:27 +0200

    added docs

--- src/aqmodels/utils.py
+++ src/aqmodels/utils.py
@@ -7,6 +7,28 @@ def quicksum(
     /,
     start: Expression | None = None,
 ) -> Expression:
+    """
+    Create an Expression based on an iterable of Expression, Variable, int or float elements.
+    Note that either the `iterable` must contain at least one `Expression` or `Variable` or 
+    the start parameter is set.
+
+    Parameters
+    ----------
+    iterable : Iterable[Expression | Variable | int | float]
+        The iterable of elements to sum up.
+    start : Expression | None, optional
+        The starting value for the summation.
+
+    Returns
+    -------
+    Expression
+        The expression created based on the sum of the iterable elements.
+
+    Raises
+    ------
+    TypeError
+        If the `iterable` does not contain any Expression or Variable.
+    """
     items = list(iterable)
     if start is None:
         for item in items:
@@ -15,8 +37,10 @@ def quicksum(
                 break
 
     if start is None:
-        raise TypeError("iterable must contain at least one Expression or Variable, or 'start' needs to be set.")
-    
+        raise TypeError(
+            "iterable must contain at least one Expression or Variable, or 'start' needs to be set."
+        )
+
     assert start is not None
     assert isinstance(start, Expression)
 
