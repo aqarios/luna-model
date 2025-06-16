31a995708ea5b2242808e6b31d97134b07524f2c
Benjamin Sühling <83377820+bsuehling@users.noreply.github.com>
2025-06-16 16:45:12 +0200

    Fix problems with bounds in version test

--- pytests/test_core/test_serialization/test_versions/test_v1/test_sol.py
+++ pytests/test_core/test_serialization/test_versions/test_v1/test_sol.py
@@ -1,9 +1,10 @@
+import time
 from pathlib import Path
 
 import numpy as np
 import pytest
 
-from aqmodels import Variable, Vtype, Model, Solution, Environment, Sense
+from aqmodels import Variable, Vtype, Model, Solution, Environment, Sense, Timer
 
 path = Path("./pytests/test_core/test_serialization/test_versions/data/v0/sol/")
 
@@ -27,9 +28,9 @@ def model_samples():
     )
 
     samples = [
-        {b1: 0, b2: 1, s1: -1, s2: 1, i1: -3, i2: 42, r1: 0, r2: 12.34},
-        {b1: 0, b2: 0, s1: -1, s2: -1, i1: -4, i2: 38, r1: 0.34, r2: 11.7923},
-        {b1: 1, b2: 1, s1: -1, s2: 1, i1: 0, i2: 12, r1: -23.1, r2: 0.001},
+        {b1: 0, b2: 1, s1: -1, s2: 1, i1: 3, i2: 42, r1: 0, r2: 12.34},
+        {b1: 0, b2: 0, s1: -1, s2: -1, i1: 4, i2: 38, r1: 0.34, r2: 11.7923},
+        {b1: 1, b2: 1, s1: -1, s2: 1, i1: 0, i2: 12, r1: 0.001, r2: 23.4}
     ]
 
     return model, samples
@@ -37,10 +38,10 @@ def model_samples():
 
 def test_v0_v1_equality_empty_sol():
     with Environment():
-        sol_empty = Solution.from_dict({})
+        sol = Solution.from_dict({})
     with open(path / "sol_empty", "rb") as f:
         reconstructed = Solution.decode(f.read())
-        assert reconstructed == sol_empty
+        assert reconstructed == sol
 
 
 def test_v0_v1_equality_sol_no_eval(
@@ -71,21 +72,25 @@ def test_v0_v1_equality_sol_sense_max(
     sol = Solution.from_dicts(samples, model=model)
     with open(path / "sol_eval_sense_max", "rb") as f:
         reconstructed = Solution.decode(f.read())
-        print()
-        print(reconstructed)
-        print("---------")
-        print(sol)
-        print("---------")
-        print(reconstructed.sense)
-        print(sol.sense)
-        # assert reconstructed == sol
+        # print()
+        # print(reconstructed)
+        # print("---------")
+        # print(sol)
+        # print("---------")
+        # print(reconstructed.sense)
+        # print(sol.sense)
+        assert reconstructed == sol
 
 
 def test_v0_v1_equality_sol_eval_timing(
         model_samples: tuple[Model, list[dict[Variable, int | float]]],
 ):
     model, samples = model_samples
-    sol = Solution.from_dicts(samples, model=model, counts=[2, 1, 1])
+    timer = Timer.start()
+    time.sleep(1.23)
+    timing = timer.stop()
+    timing.qpu = 0.0142
+    sol = Solution.from_dicts(samples, model=model, counts=[2, 1, 1], timing=timing)
     with open(path / "sol_eval_timing", "rb") as f:
         reconstructed = Solution.decode(f.read())
         assert reconstructed.variable_names == sol.variable_names
@@ -93,6 +98,6 @@ def test_v0_v1_equality_sol_eval_timing(
         assert np.all(reconstructed.obj_values == sol.obj_values)
         assert np.all(reconstructed.counts == sol.counts)
         assert np.all(reconstructed.raw_energies == sol.raw_energies)
-        assert reconstructed.runtime.qpu == 0.0142
+        assert reconstructed.runtime.qpu == sol.runtime.qpu
         total_seconds = reconstructed.runtime.total_seconds
         assert total_seconds > 1.23 and total_seconds < 1.231
