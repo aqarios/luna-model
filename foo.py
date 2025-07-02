34533839da0035bb76f9330b29d85cb2f75fa442
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-07-02 12:28:23 +0200

    verified working

--- foo.py
+++ foo.py
@@ -16,5 +16,11 @@ ir = pm.run(model)
 
 print(ir.model)
 sol = Solution.from_dict({"x_s": 0, "x": 1}, model=ir.model)
-
 print(sol)
+
+
+# try with owned
+res = ir.model.evaluate_sample(sol.samples[0])
+print(res)
+res2 = ir.model.evaluate_sample(res.sample)
+print(res2)
