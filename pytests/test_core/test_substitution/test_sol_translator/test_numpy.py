801671ed4097010eaf0a8c4cde7b04b2117b7ea2
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-07-03 09:56:25 +0200

    some translator tests for model with substituion

---
+++ pytests/test_core/test_substitution/test_sol_translator/test_numpy.py
@@ -0,0 +1,21 @@
+import pytest
+import numpy as np
+from aqmodels.translator import NumpyTranslator
+from .common import do_checks  # type: ignore[reportMissingImports]
+
+
+@pytest.fixture
+def np_solution() -> tuple[np.typing.NDArray, np.typing.NDArray]:
+    return (
+        np.array(
+            [
+                [0, 1, -1, +1, 4, 3],
+            ]
+        ),
+        np.array([3.14]),
+    )
+
+
+def test_dwave_sol_with_substituted_model(np_solution: tuple[np.typing.NDArray, np.typing.NDArray]):
+    do_checks(NumpyTranslator, np_solution)
+
