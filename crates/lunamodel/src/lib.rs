1338fb8daf48213e54d346ed3639fac1e873ed8c
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-11-14 09:57:13 +0100

    layout start

--- crates/lunamodel/src/lib.rs
+++ crates/lunamodel/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+//! # LunaModel: Symbolic modeling for optimization
+//!
+//! LunaModel is a high-performance symbolic modeling library for describing, translating and 
+//! transforming optimization problems. It provides the following high-level features:
+//!     - System for defining symbolic algebraic expressions of arbitrary degree, constraints and 
+//!       optimization models (like dimod, gurobi or cplex)
+//!     - Translations from and to an LunaModel for many common optimization model formats (like LP)
+//!     - Transformations to map an LunaModel from a general model to a specific model, such as transforming a 
+//!       Constrained (Binary) Quadratic Model (CQM) to a (Unconstrained) Binary Quadratic Model (BQM), 
+//!       or from an Integer Model to a Binary Model.
+//!     - Builtin serialization for maximum portability
+//!     - Python-first development experience
+//!
+//! You can use LunaModel as a standalone package or by using [luna-quantum](https://pypi.org/project/luna-quantum/) 
+//! which gives you additional builtin primitives to solve your optimization problems using the [Luna Platform](https://aqarios.com/platform).
