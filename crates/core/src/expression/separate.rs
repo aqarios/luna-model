86a6847a6b9bc04c90a0230c89c3e72f5d04defd
jflxb <19431952+JFLXB@users.noreply.github.com>
2025-12-09 16:39:47 +0100

    added separation

--- crates/core/src/expression/separate.rs
+++ crates/core/src/expression/separate.rs
@@ -1,9 +1,57 @@
+use hashbrown::HashSet;
+use itertools::Itertools;
+use lunamodel_error::{LunaModelError, LunaModelResult};
+use lunamodel_types::VarIdx;
+
 use super::Expression;
 use crate::variable::VarRef;
 
 impl Expression {
-    pub fn separate(&self, vars: &[VarRef]) -> (Expression, Expression) {
-        _ = vars;
-        todo!()
+    pub fn separate(&self, vrefs: &[VarRef]) -> LunaModelResult<(Expression, Expression)> {
+        let mut left = Expression::empty(self.env.clone());
+        let mut right = Expression::empty(self.env.clone());
+        right.offset = self.offset;
+
+        if !vrefs.iter().all(|v| v.env.id() == self.env.id()) {
+            return Err(LunaModelError::DifferentEnvironments);
+        }
+
+        let set: HashSet<VarIdx> = HashSet::from_iter(vrefs.iter().map(|x| x.id));
+
+        for (x, v) in self.linear_items() {
+            if v == 0.0 {
+                continue;
+            }
+            if set.contains(&x.id) {
+                left.linear += (x.id, v);
+            } else {
+                right.linear += (x.id, v);
+            }
+        }
+        for (x, y, v) in self.quadratic_items() {
+            if v == 0.0 {
+                continue;
+            }
+            if set.contains(&x.id) || set.contains(&y.id) {
+                left.quadratic.as_mut().map(|q| *q += (x.id, y.id, v));
+            } else {
+                right.quadratic.as_mut().map(|q| *q += (x.id, y.id, v));
+            }
+        }
+        for (xs, v) in self.higher_order_items() {
+            if v == 0.0 {
+                continue;
+            }
+            let xs = xs.iter().map(|v| v.id).collect_vec();
+            if xs.iter().any(|x| set.contains(x)) {
+                left.higher_order.as_mut().map(|q| *q += (xs.as_slice(), v));
+            } else {
+                right
+                    .higher_order
+                    .as_mut()
+                    .map(|q| *q += (xs.as_slice(), v));
+            }
+        }
+        Ok((left, right))
     }
 }
--- crates/core/src/variable/var.rs
+++ crates/core/src/variable/var.rs
@@ -8,23 +8,16 @@ pub struct Variable {
     pub(crate) name: VarName,
     pub(crate) vtype: Vtype,
     pub(crate) bounds: Bounds,
-    // pub(crate) envid: EnvIdx,
     pub inverted: Option<VarIdx>,
 }
 
 impl Variable {
-    pub fn new(
-        name: &str,
-        vtype: Vtype,
-        bounds: Option<LazyBounds>,
-        // envid: EnvIdx,
-    ) -> LunaModelResult<Self> {
+    pub fn new(name: &str, vtype: Vtype, bounds: Option<LazyBounds>) -> LunaModelResult<Self> {
         let bounds = bounds.concretize(&vtype)?;
         Ok(Self {
             name: name.into(),
             vtype,
             bounds,
-            // envid,
             inverted: None,
         })
     }
@@ -43,11 +36,4 @@ impl Variable {
     pub fn bounds(&self) -> &Bounds {
         &self.bounds
     }
-
-    // pub fn envid(&self) -> EnvIdx {
-    //     self.envid
-    // }
-    // pub fn inverted(&self) -> Option<VarIdx> {
-    //     self.inverted
-    // }
 }
