a0c144d249b2fcbd3f80e8b66fb3386ae8bbd142
jflxb <19431952+JFLXB@users.noreply.github.com>
2026-02-03 10:52:47 +0100

    more verification tests (#456)

--- py-lunamodel/tests/extra/solution/test_sbest.py
+++ py-lunamodel/tests/extra/solution/test_sbest.py
@@ -12,12 +12,31 @@ def model() -> Model:
     return model
 
 @pytest.fixture
+def model_min() -> Model:
+    model = Model()
+    x = model.add_variable("x", Vtype.BINARY)
+    y = model.add_variable("y", Vtype.BINARY)
+    model.set_objective(x + y)
+    model.sense = Sense.MIN
+    return model
+
+@pytest.fixture
 def constr_model() -> Model:
     model = Model()
     x = model.add_variable("x", Vtype.BINARY)
     y = model.add_variable("y", Vtype.BINARY)
     model.set_objective(x + y)
     model.sense = Sense.MAX
+    model.add_constraint(x + y <= 1)
+    return model
+
+@pytest.fixture
+def constr_model_min() -> Model:
+    model = Model()
+    x = model.add_variable("x", Vtype.BINARY)
+    y = model.add_variable("y", Vtype.BINARY)
+    model.set_objective(x + y)
+    model.sense = Sense.MIN
     model.add_constraint(x + y <= 1)
     return model
 
@@ -49,6 +68,28 @@ def test_solution_best_constr(constr_model: Model):
 
 def test_solution_best_constr_eq(constr_model: Model):
     solution = build_sol_eq(constr_model)
+    best_solutions = solution.best()
+    assert best_solutions is not None
+    assert len(best_solutions) == 2
+    assert 1 == best_solutions[0].obj_value
+    assert 1 == best_solutions[1].obj_value
+
+def test_solution_best_min(model_min: Model):
+    solution = build_sol(model_min)
+    best_solutions = solution.best()
+    assert best_solutions is not None
+    assert len(best_solutions) == 1
+    assert 1 == best_solutions[0].obj_value
+
+def test_solution_best_constr_min(constr_model_min: Model):
+    solution = build_sol(constr_model_min)
+    best_solutions = solution.best()
+    assert best_solutions is not None
+    assert len(best_solutions) == 1
+    assert 1 == best_solutions[0].obj_value
+
+def test_solution_best_constr_eq_min(constr_model_min: Model):
+    solution = build_sol_eq(constr_model_min)
     best_solutions = solution.best()
     assert best_solutions is not None
     assert len(best_solutions) == 2
