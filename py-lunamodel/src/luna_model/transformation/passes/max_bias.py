87ca50345b44d7d429a15e023b008843a4fe4701
jflxb <19431952+JFLXB@users.noreply.github.com>
2026-02-04 11:46:58 +0100

    fxies

--- py-lunamodel/src/luna_model/transformation/passes/max_bias.py
+++ py-lunamodel/src/luna_model/transformation/passes/max_bias.py
@@ -6,11 +6,4 @@ class MaxBiasAnalysis(ConcreteAnalysisPass):
     """An analysis pass computing the maximum bias contained in the model."""
 
     def __init__(self) -> None:
-        """Transform the model's Sense to a target Sense.
-
-        Parameters
-        ----------
-        sense : Sense
-            The target sense of the model after calling the `run` method on it.
-        """
         super().__init__(base=PyMaxBiasAnalysis())
