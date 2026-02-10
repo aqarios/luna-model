aee20683e543f62a3c662a6f7a45abb3939afd51
jflxb <19431952+JFLXB@users.noreply.github.com>
2026-02-10 12:02:59 +0100

    makes v1 decodable for smooth transition preperation for #474

--- crates/serializer/src/encodables/environment.rs
+++ crates/serializer/src/encodables/environment.rs
@@ -1,7 +1,7 @@
 use crate::encode::{Decodable, Decoder, Encodable};
 use crate::versionize::{Version, Versioned};
 use crate::versions::v0::SerEnvironment as SerEnvV0;
-// use crate::versions::v1::SerEnvironment as SerEnvV1;
+use crate::versions::v1::SerEnvironment as SerEnvV1;
 
 use lunamodel_core::Environment;
 use lunamodel_error::LunaModelResult;
@@ -10,6 +10,7 @@ use lunamodel_error::LunaModelResult;
 /// of an [Environment]. In case a new serialization format is defined update this value
 /// to ensure all uses of serialization throught the entire library use the most recent
 /// serialization implementation.
+// TODO: delete until TODO(@HERE) and uncomment from TODO(@HERE) for V1 activation.
 type SerEnvLatest = SerEnvV0;
 /// Makes an [Environment] encodable.
 impl Encodable<SerEnvV0> for Environment {
@@ -17,7 +18,8 @@ impl Encodable<SerEnvV0> for Environment {
         Version::V0
     }
 }
-
+impl Decoder<Environment, ()> for SerEnvV1 {}
+// TODO(@HERE): delete ABOVE code and activate below code for V1 activation.
 // type SerEnvLatest = SerEnvV1;
 // /// Makes an [Environment] encodable.
 // impl Encodable<SerEnvV1> for Environment {
@@ -25,6 +27,7 @@ impl Encodable<SerEnvV0> for Environment {
 //         Version::V1
 //     }
 // }
+// impl Decoder<Environment, ()> for SerEnvV0 {}
 
 /// Default implementation to make a bytes vector deserializable to an [Environment].
 impl Decodable<Environment> for Vec<u8> {
@@ -39,7 +42,7 @@ impl Decodable<Environment> for Versioned<Vec<u8>> {
     fn decode(&self, payload: Self::Payload) -> LunaModelResult<Environment> {
         match self.version {
             Some(Version::V0) => SerEnvV0::decoder(self.data.as_slice(), payload),
-            // Some(Version::V1) => SerEnvV1::decoder(self.data.as_slice(), payload),
+            Some(Version::V1) => SerEnvV1::decoder(self.data.as_slice(), payload),
             _ => SerEnvLatest::decoder(self.data.as_slice(), payload),
         }
     }
--- crates/serializer/src/encodables/expression.rs
+++ crates/serializer/src/encodables/expression.rs
@@ -1,7 +1,7 @@
 use crate::encode::{Decodable, Decoder, Encodable};
 use crate::versionize::{Version, Versioned};
 use crate::versions::v0::SerExpression as SerExprV0;
-// use crate::versions::v1::SerExpression as SerExprV1;
+use crate::versions::v1::SerExpression as SerExprV1;
 
 use lunamodel_core::{ArcEnv, Expression};
 use lunamodel_error::LunaModelResult;
@@ -10,6 +10,7 @@ use lunamodel_error::LunaModelResult;
 /// of an [Expression]. In case a new serialization format is defined update this value
 /// to ensure all uses of serialization throught the entire library use the most recent
 /// serialization implementation.
+// TODO: delete until TODO(@HERE) and uncomment from TODO(@HERE) for V1 activation.
 type SerExprLatest = SerExprV0;
 /// Makes an [Expression] encodable.
 impl Encodable<SerExprV0> for Expression {
@@ -17,7 +18,8 @@ impl Encodable<SerExprV0> for Expression {
         Version::V0
     }
 }
-
+impl Decoder<Expression, ArcEnv> for SerExprV1 {}
+// TODO: delete ABOVE code and activate below code for V1 activation.
 // type SerExprLatest = SerExprV1;
 // /// Makes an [Expression] encodable.
 // impl Encodable<SerExprV1> for Expression {
@@ -25,6 +27,7 @@ impl Encodable<SerExprV0> for Expression {
 //         Version::V1
 //     }
 // }
+// impl Decoder<Expression, ArcEnv> for SerExprV0 {}
 
 /// Default implementation to make a bytes vector ([Vec<u8>]) deserializable to an [Expression].
 /// For the decoding of a [Vec<u8>] to an [Expression] a pointer to
@@ -43,7 +46,7 @@ impl Decodable<Expression> for Versioned<Vec<u8>> {
     fn decode(&self, payload: Self::Payload) -> LunaModelResult<Expression> {
         match self.version {
             Some(Version::V0) => SerExprV0::decoder(self.data.as_slice(), payload),
-            // Some(Version::V1) => SerExprV1::decoder(self.data.as_slice(), payload),
+            Some(Version::V1) => SerExprV1::decoder(self.data.as_slice(), payload),
             _ => SerExprLatest::decoder(self.data.as_slice(), payload),
         }
     }
