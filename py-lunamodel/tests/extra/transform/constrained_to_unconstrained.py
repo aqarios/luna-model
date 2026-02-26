7e9c8cee77768735fa3b8f1690373fac17d7f63e
jflxb <19431952+JFLXB@users.noreply.github.com>
2026-02-26 17:32:14 +0100

    added simple model test

--- py-lunamodel/tests/extra/transform/constrained_to_unconstrained.py
+++ py-lunamodel/tests/extra/transform/constrained_to_unconstrained.py
@@ -1,31 +1,37 @@
-import pytest
-
-from luna_model import Model, Vtype
+from luna_model import Model
 from luna_model.transformation import PassManager, pipelines
 
-constrained_model = Model("constrained_model")
-x = constrained_model.add_variable("x")
-y = constrained_model.add_variable("y")
-z = constrained_model.add_variable("z")
+def test_very_simple_model():
+    model = Model()
+    x = model.add_variable("x")
+    y = model.add_variable("y")
+    z = model.add_variable("z")
+    
+    model.objective = x + y + z
+    
+    model.constraints += x + y + z <= 3
+    model.constraints += x - y - z >= 0
+    model.constraints += x + y == 2
+    
+    penalty_factor = 10
+    pm = PassManager([pipelines.ConstrainedToUnconstrainedPipeline(penalty_factor=penalty_factor)])
+    ir = pm.run(model)
 
-constrained_model.objective = 2 * x * z + 3 * x * y + 4 * y * z + 10 * x + 3 * y + 12 * z - 12
+    assert 6 == len(ir.model.variables())
+    assert 0 == len(ir.model.constraints)
 
-constrained_model.constraints += x + y + z <= 3
-constrained_model.constraints += x - y - z >= 0
-constrained_model.constraints += x + y == 2
+    x, y, z, slack_c1_b0, slack_c0_b0, slack_c0_b1 = (
+        ir.model.get_variable("x"),
+        ir.model.get_variable("y"),
+        ir.model.get_variable("z"),
+        ir.model.get_variable("slack_c1_b0"),
+        ir.model.get_variable("slack_c0_b0"),
+        ir.model.get_variable("slack_c0_b1"),
+    )
 
-
-constr_to_unconstr_pipeline = pipelines.ConstrainedToUnconstrainedPipeline()
-
-print(constr_to_unconstr_pipeline)
-
-pm = PassManager([constr_to_unconstr_pipeline])# penalty_factor=10
-ir = pm.run(constrained_model)
-unconstrained_model = ir.model
-unconstrained_model.name = "unconstrained_model"
-print(unconstrained_model.objective)
-# 2 x z + 3 x y + 4 y z + 10 x + 3 y + 12 z - 12 +
-#  120 (x + y + z + slack_c0_b0 + 2*slack_c0_b1 )^2 + 120 (-x + y + z + slack_c1_b0 -1 )^2 + 120 (x + y - 2)^2
-print(len(unconstrained_model.constraints))
-print()
-print(unconstrained_model)
+    obj = x + y + z
+    c0_penalty = (x + y + z + slack_c0_b0 + 2 * slack_c0_b1)**2
+    c1_penalty = (-1 * (x - y - z) + slack_c1_b0)**2
+    c2_penalty = (x + y - 2)**2
+    expected_objective = obj + penalty_factor * (c0_penalty + c1_penalty + c2_penalty)
+    assert expected_objective.equal_contents(ir.model.objective)
