3923e7ba126bdbbcabf89d725a0d4f84313aab70
jflxb <19431952+JFLXB@users.noreply.github.com>
2026-04-07 13:35:12 +0200

    port: ge to le constraints

---
+++ crates/transformv2/src/transformation/ge_to_le/pass.rs
@@ -0,0 +1,37 @@
+use lunamodel_core::{Model, Solution, ops::LmMulAssign};
+use lunamodel_error::LunaModelResult;
+use lunamodel_transpiler::{PassContext, ReversiblePass};
+use lunamodel_types::Comparator;
+
+use super::artifact::GeToLeConstraintsArtifact as GTLCArtifact;
+
+pub struct GeToLeConstraintsPass;
+
+impl GeToLeConstraintsPass {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl ReversiblePass for GeToLeConstraintsPass {
+    type Artifact = GTLCArtifact;
+
+    fn name(&self) -> &str {
+        "ge-to-le-constraints"
+    }
+
+    fn forward(&self, model: &mut Model, _ctx: &PassContext) -> LunaModelResult<Self::Artifact> {
+        for (_, constraint) in model.constraints.iter_mut() {
+            if constraint.comparator == Comparator::Ge {
+                constraint.lhs.mul_assign(-1.0)?;
+                constraint.rhs *= -1.0;
+                constraint.comparator = Comparator::Le;
+            }
+        }
+        Ok(Self::Artifact {})
+    }
+
+    fn backward(_artifact: &Self::Artifact, solution: Solution) -> LunaModelResult<Solution> {
+        Ok(solution)
+    }
+}
--- crates/transformv2/src/transformation/mod.rs
+++ crates/transformv2/src/transformation/mod.rs
@@ -1,7 +1,9 @@
 mod binary_spin;
 mod change_sense;
 mod eq_constr_to_quad_pen;
+mod ge_to_le;
 
 pub use binary_spin::BinarySpinPass;
 pub use change_sense::ChangeSensePass;
 pub use eq_constr_to_quad_pen::EqualityConstraintsToQuadraticPenaltyPass;
+pub use ge_to_le::GeToLeConstraintsPass;
