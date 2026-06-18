de64023443e074580223c01cb522d7ed33b0b0a5
jflxb <19431952+JFLXB@users.noreply.github.com>
2026-06-18 12:24:47 +0200

    fix: rust tests (#4)

--- crates/transform/src/transformation/change_sense/mod.rs
+++ crates/transform/src/transformation/change_sense/mod.rs
@@ -49,6 +49,8 @@ mod tests {
 
     #[test]
     fn roundtrip_change_sense_pass_to_min() -> LunaModelResult<()> {
+        register_backward::<ChangeSensePass>();
+
         let mut model = Model::default();
         let x = model.add_var("i0", Vtype::Integer, None)?;
         let y = model.add_var("i1", Vtype::Integer, None)?;
